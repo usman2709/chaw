188532a81ae8c7ef391e44a40bed7e2b7b45ba8e
usman2709 <usman2709@yahoo.com>
2022-09-28 10:43:31 +0100

    comment here

--- chaw/settings.py
+++ chaw/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z0gv5_noq2mkj6ad8(!ey-&uh^=_ki3n1iqmj42s%($4mj0)ru'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['3.83.92.98']
